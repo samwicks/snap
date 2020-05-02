f152c0773361edebf68cdf1bff46f1aa141efab0
Sam Wicks <samwicks@users.noreply.github.com>
2020-05-03 00:42:53 +0100

    cards variable is now a linked list

--- src/snap/Deck.java
+++ src/snap/Deck.java
@@ -1,45 +1,41 @@
 package snap;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Deck {
 
-    private final List<Card> cards = new ArrayList<>();
-    private final List<String> validNumbers;
-    private final List<Character> validSuits;
+//    private final List<Card> cards = new ArrayList<>();
+    private final LinkedList<Card> cards = new LinkedList<>();
+    private final List<String> values;
+    private final List<Character> suits;
 
     /**
      * Generate default deck of cards on construction
      */
     public Deck () {
-        validNumbers = Arrays.asList("A","2","3","4","5","6","7","8","9","10","J","Q","K");
-        validSuits = Arrays.asList('C','D','H','S');
+        values = Arrays.asList("A","2","3","4","5","6","7","8","9","10","J","Q","K");
+        suits = Arrays.asList('C','D','H','S');
         generateDeck();
     }
 
     /**
-     * Generate custom deck of cards with given numbers and suits
+     * Generate custom deck of cards with given values and suits
      *
-     * @param numbers List of valid card numbers
+     * @param values List of valid card values
      * @param suits List of valid suits
      */
-    public Deck (List<String> numbers, List<Character> suits) {
-        validNumbers = numbers;
-        validSuits = suits;
+    public Deck (List<String> values, List<Character> suits) {
+        this.values = values;
+        this.suits = suits;
     }
 
     /**
-     * Generate a deck of cards with given numbers and suits
+     * Generate a deck of cards with given values and suits
      */
     private void generateDeck() {
-        int count = 0;
-        for (Character s : validSuits) {
-            for (String n : validNumbers) {
+        for (Character s : suits) {
+            for (String n : values) {
                 cards.add(new Card(n, s));
-                count++;
             }
         }
     }
@@ -47,7 +43,7 @@ public class Deck {
     /**
      * @return List of all cards in the deck
      */
-    public List<Card> getDeck() {
+    public LinkedList<Card> getDeck() {
         return cards;
     }
 
@@ -64,17 +60,8 @@ public class Deck {
      * @return Top card in deck
      */
     public Card deal() {
-        // Return null if no cards left in deck
-        Card result = null;
-
-        // Check if deck has any cards left to deal
-        if (!getDeck().isEmpty()) {
-            // Return top card in deck
-            Card card = getDeck().get(0);
-            cards.remove(0);
-            result = card;
-        }
-        return result;
+        // Return top card in deck
+        return cards.pop();
     }
 
 }
