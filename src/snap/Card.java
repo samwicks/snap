e451b6bf48cfed0366c622d2483928cb096fcd35
Sam Wicks <samwicks@users.noreply.github.com>
2020-05-03 00:40:32 +0100

    Renamed variables for better readability

--- src/snap/Card.java
+++ src/snap/Card.java
@@ -1,48 +1,48 @@
 package snap;
 
 public class Card {
-    private final String cardNumber;
-    private final char cardSuit;
+    private final String value;
+    private final char suit;
 
     /**
-     * @param number Number of card
+     * @param value  Value of card
      * @param suit   Suit of card
      */
-    public Card(String number, Character suit) {
+    public Card(String value, char suit) {
         // Set card values at creation
-        cardNumber = number;
-        cardSuit = suit;
+        this.value = value;
+        this.suit = suit;
     }
 
     /**
-     * @return Number of card
+     * @return Value of card
      */
-    public String getNumber() {
-        return this.cardNumber;
+    public String getValue() {
+        return this.value;
     }
 
     /**
      * @return Suit of card
      */
     public char getSuit() {
-        return this.cardSuit;
+        return this.suit;
     }
 
     /**
      * @return Card value
      */
     public String toString() {
-        return "Card[" + this.getNumber() + "," + this.getSuit() + "]";
+        return "Card[" + this.getValue() + "," + this.getSuit() + "]";
     }
 
     /**
      * @return Easily readable value of card
      */
     public String display() {
-        return String.format("%s%c", this.getNumber(), this.getSuit());
+        return String.format("%s%c", this.getValue(), this.getSuit());
     }
 
     public boolean equalValue(Card card) {
-        return this.getNumber().equals(card.getNumber());
+        return this.getValue().equals(card.getValue());
     }
 }
